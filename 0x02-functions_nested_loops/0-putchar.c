c0b447f8db508c446f2b0c83255147d0742d9651
Andu <yyou@examle.com>
2022-07-06 15:04:41 -0700

    fixed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,12 +9,12 @@ int main(void)
 	_putchar('_');
 	_putchar('P');
 	_putchar('u');
-       	_putchar('t');
-       	_putchar('c');
-       	_putchar('h');
-       	_putchar('a');
-       	_putchar('r');
-       	_putchar('\n');
-       
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	
 	return(0);
 }
