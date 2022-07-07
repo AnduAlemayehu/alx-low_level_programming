56982defa971250a6fb70d5974c965971c1a8fe1
Andu <yyou@examle.com>
2022-07-06 17:16:39 -0700

    hhhhh

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *   * main - Prints the first 52 fibonacci numbers
+ *
+ *     *
+ *
+ *       * Return: Nothing!
+ *
+ */
+
+int main(void)
+
+{
+
+		int i = 0;
+
+			long j = 1, k = 2;
+
+
+
+				while (i < 50)
+
+						{
+
+									if (i == 0)
+
+													_putchar("%ld", j);
+
+											else if (i == 1)
+
+															_putchar(", %ld", k);
+
+													else
+
+																{
+
+																				k += j;
+
+																							j = k - j;
+
+																										printf(", %ld", k);
+
+																												}
+
+
+
+															++i;
+
+																}
+
+
+
+					printf("\n");
+
+						return (0);
+
+}
