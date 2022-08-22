879fc2f1d4e10de4c59728759124f0c71f19cc75
Andu <you@example.com>
2022-08-22 15:36:33 -0700

    fix o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,64 +1,69 @@
-#include <stdio.h>
-
-#include <stdlib.h>
-
 #include "main.h"
 
 
 
 /**
-  * read_textfile - that reads a text file and print
-  * @filename: variable pointer
-  * @letters: size letters
-  * Description: Write a function that reads a text file and prints it
-  * to the POSIX standard output.
-  * Return: the actual number of letters it could read and print, 0 otherwise
-  * /
-
-
+  * read_textfile - read a text file and print it to stdout
+  * @filename: the name of the file to read
+  * @letters: the number of letters to be read and printed
+  *
+  * Return: If filename is NULL, the file cannot be opened or read, or
+  *  write fails or returns an unexpected number of bytes, return 0.
+  * Otherwise, return the actual number of letters read and printed.
+  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 
 {
 
-	ssize_t file, let, w;
+	char *buffer = NULL;
 
-	char *text;
+	ssize_t b_read;
 
+	ssize_t b_written;
 
+	int fd;
 
-	text = malloc(letters);
-
-	if (text == NULL)
-
-		return (0);
-
-
-
-	if (filename == NULL)
+	if (!(filename && letters))
 
 		return (0);
 
+	fd = open(filename, O_RDONLY);
 
+	if (fd == -1)
 
-	file = open(filename, O_RDONLY);
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
 
+	if (!buffer)
 
+		return (0);
 
-	if (file == -1)
+	b_read = read(fd, buffer, letters);
+
+	close(fd);
+
+	if (b_read < 0)
 
 	{
-	free(text);
+
+		free(buffer);
 
 		return (0);
 
 	}
 
-	let = read(file, text, letters);
+	if (!b_read)
 
-	w = write(STDOUT_FILENO, text, let);
-	close(file);
+		b_read = letters
 
-	return (w);
+	b_written = write(STDOUT_FILENO, buffer, b_read);
+
+	free(buffer);
+	if (b_written < 0)
+
+		return (0);
+
+	return (b_written);
 
 }
